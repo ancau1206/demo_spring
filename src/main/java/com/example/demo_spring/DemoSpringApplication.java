c6c31eed03d841ec0d000f0bf671fa89efb9206a
DESKTOP-QBLRO6P\HLC_2021 <cauav@osp.vn>
2023-07-13 10:31:36 +0700

    oke

--- src/main/java/com/example/demo_spring/DemoSpringApplication.java
+++ src/main/java/com/example/demo_spring/DemoSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoSpringApplication {
 
 	public static void main(String[] args) {
+		System.out.println("demo 1111111111111111111111111");
 		SpringApplication.run(DemoSpringApplication.class, args);
 	}
 
